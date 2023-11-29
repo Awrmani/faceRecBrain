97b9c1e8f9f77b199df7b6486afcd725f34c890c
Awrmani <armanvalaee@gmail.com>
2023-11-29 01:55:25 -0500

    homepage style upgrade

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+// import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import { render } from '@testing-library/react';
 
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 30,
+//       density: {
+//         enable: true,
+//         value_area: 500
+//       }
+//     }
+//   }
+// }
 class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* <Particles 
+        params={particlesOptions}
+        /> */}
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*<FaceRecognition /> */}
       </div>
